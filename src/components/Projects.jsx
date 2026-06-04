c56e46d723c658d84a80d287fe995eae97cb6f04
yxonsz <yxxn_su@naver.com>
2026-06-04 21:09:19 +0900

    프로젝트 추가

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,15 @@ const projects = [
     desc: "MySQL과 Java Swing GUI를 활용해 기숙사 세탁기를 예약·취소할 수 있는 프로그램. 콘솔과 GUI 두 가지 방식으로 구현.",
     tags: ["Java", "Java Swing", "MySQL"],
     period: "2025.07.08 - 2025.07.18",
-    link: "https://velog.io/@ye0n_s3/Java-%EC%9E%90%EB%B0%94-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EA%B8%B0%EC%88%99%EC%82%AC-%EC%84%B8%ED%83%81%EA%B8%B0-%EC%98%88%EC%95%BD",
+    link: "https://velog.io/@ye0n_s3/Java-자바-프로젝트-기숙사-세탁기-예약",
+  },
+  {
+    num: "002",
+    title: "반려동물 관리 웹",
+    desc: "Java Script와 MySQL을 활용해 반려동물을 관리하는 프로그램. 반려동물 정보 등록, 일정 등록, 메모 기능 제공.",
+    tags: ["JavaScript", "MySQL"],
+    period: "2025.05.12 - 2025.06.04",
+    link: "https://velog.io/@ye0n_s3/DBP-DB-프로젝트-반려동물-관리-웹",
   },
 ];
 
